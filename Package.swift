6e229abe4ed59f31051810b698f0470c45de5382
Carlos Jimenez <carlos.jimenez@klarna.com>
2020-09-25 10:30:24 +0200

    Added targets to SPM

--- Package.swift
+++ Package.swift
@@ -6,8 +6,19 @@ import PackageDescription
 let package = Package(
     name: "KlarnaCheckoutSDK",
     products: [
-       .library(
-           name: "KlarnaCheckoutSDK",
-           targets: ["KlarnaCheckoutSDK"]),
-     ]
+        .library(
+            name: "KlarnaCheckoutSDK",
+            targets: ["KlarnaCheckoutSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "KlarnaCheckoutSDK",
+            dependencies: [])
+    ]
 )
