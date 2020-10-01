5c7ceef8d373e1ac575e4d20c26fad7bbd1708c6
Carlos Jimenez <carlos.jimenez@klarna.com>
2020-10-01 12:25:40 +0200

    Update target

--- Package.swift
+++ Package.swift
@@ -17,11 +17,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "KlarnaCheckoutSDK",
-            path: "ios",
-            exclude: ["README.md"]
-        ),
         .binaryTarget(
             name: "KlarnaCheckoutSDK",
             path: "ios/XCFramework/KlarnaCheckoutSDK.zip"
